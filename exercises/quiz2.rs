9fd5ee037e6915725b7b9d8c64ef024fcb607bb5
freekeeper <8000116021@email.ncu.edu.cn>
2023-08-18 19:05:48 +0800

    finish quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -32,11 +31,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command{
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(n) => {
+                    let mut appended_string = string.clone();
+                    for _ in 0..*n {
+                        
+                        appended_string.push_str("bar");
+                    }
+                    output.push(appended_string);
+                }
+            }
         }
         output
     }
@@ -45,7 +59,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
@@ -60,5 +74,6 @@ mod tests {
         assert_eq!(output[1], "all roads lead to rome!");
         assert_eq!(output[2], "foobar");
         assert_eq!(output[3], "barbarbarbarbarbar");
+
     }
 }
